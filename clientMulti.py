396434dd9ee8cf1b18430ff7d5e64aca9e599d8c
Melody Victorian <50192955+melodyvictorian22@users.noreply.github.com>
2020-03-27 20:33:33 +0700

    change .send to .sendall

--- clientMulti.py
+++ clientMulti.py
@@ -3,13 +3,13 @@ SERVER = "127.0.0.1"
 PORT = 8080
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
-name = input()
-client.send(bytes("This is from " + name,'UTF-8'))
+name = input("Please enter your name : ")
+client.sendall(bytes("This is from " + name,'UTF-8'))
 while True:
   in_data =  client.recv(1024)
   print("From Server :" ,in_data.decode())
   out_data = input()
-  client.send(bytes(out_data,'UTF-8'))
+  client.sendall(bytes(out_data,'UTF-8'))
   if out_data=='bye':
     break
 client.close()
