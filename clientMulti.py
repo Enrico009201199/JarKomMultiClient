122c9d85a84ccd2197d00235759b61d8ad312503
Melody Victorian <50192955+melodyvictorian22@users.noreply.github.com>
2020-03-31 22:10:16 +0700

    ganti koding

--- clientMulti.py
+++ clientMulti.py
@@ -1,15 +1,45 @@
 import socket
-SERVER = "127.0.0.1"
-PORT = 8080
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER, PORT))
-name = input("Please enter your name : ")
-client.sendall(bytes("This is from " + name,'UTF-8'))
+import sys
+
+def intros(s):
+    res = s.recv(PORT).decode()
+    print(res)
+
+def question(s):
+    quesNum = s.recv(PORT).decode()
+    print(quesNum)
+    ques = s.recv(PORT).decode()
+    print(ques)
+    ans = input("Jawaban: ")
+    while ans not in ['A', 'B', 'C', 'D']:
+        print ("Jawaban tidak sesuai dengan pilihan yang tersedia ! Try Again")
+        ans = input("Jawaban: ")
+    s.send(ans.encode())
+    response = s.recv(PORT).decode()
+    print(response)
+
+def scores(s):
+    res = s.recv(PORT).decode()
+    print(res)
+
+def final(s):
+    res = s.recv(PORT).decode()
+    print(res)
+
+HOST = 'localhost'   
+PORT = int(input("Input port yang akan digunakan : "))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
 while True:
-  in_data =  client.recv(1024)
-  print("From Server :" ,in_data.decode())
-  out_data = input()
-  client.sendall(bytes(out_data,'UTF-8'))
-  if out_data=='bye':
-    break
-client.close()
+    choice = s.recv(PORT).decode()
+    if choice == "I": 
+        intros(s)
+    elif choice == "Q":
+        question(s)
+    elif choice == "S":
+        scores(s)
+    elif choice == "F":
+        final(s)
+        break
+s.close()
--- serverMulti.py
+++ serverMulti.py
@@ -1,37 +1,116 @@
-import socket, threading, datetime
-class ClientThread(threading.Thread):
-    def __init__(self,clientAddress,clientsocket):
-        threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        print ("New connection added: ", clientAddress)
-    def run(self):
-        print ("Connection from : ", clientAddress)
-        msg = ''
-        while True:
-            data = self.csocket.recv(2048)
-            print(data)
-            msg = input()
-            if msg=='bye':
-              break
-            elif(msg.lower() == 'start'):
-                questions = open("questions.csv", "r")
-                ques = questions.readlines()
-                for q in ques:
-                    data = q.split(",")
-                    msg = data[1] + "\na. " + data[2] + "\nb. " + data[3] + "\nc. " + data[4] + "\nd. " + data[5] 
-                    self.csocket.sendall(bytes(msg,'UTF-8'))
-                    data = self.csocket.recv(2048)
-                    print(str(data))
-        print ("Client at ", clientAddress , " disconnected...")
-LOCALHOST = "127.0.0.1"
-PORT = 8080
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((LOCALHOST, PORT))
-server.listen(1)
-print("Server started")
-print("Waiting for client request..")
-while True:
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(clientAddress, clientsock)
-    newthread.start()
+import socket
+import threading
+import sys
+import datetime
+import time
+
+HOST = 'localhost'
+PORT = int(input("Masukkan port yang akan digunakan: "))
+
+score = [0, 0]
+totalQuestions = int(input("Jumlah pertanyaan yang ingin dijawab: "))
+waktu = [0, 0]
+filename = input("Masukkan nama file quiz : ")
+f = open(filename, 'r')
+
+cek = [False] *totalQuestions
+
+def askQuestion(connlist, playerNo, ques, ans, questionNo):
+    connlist[playerNo].send(ques.encode())
+    time.sleep(0.1)
+
+    data = connlist[playerNo].recv(PORT).decode()
+    waktu[playerNo] = datetime.datetime.now()                    #receive answer
+    if ans.rstrip() == data :
+        if f[questionNo] == False :
+            score[playerNo]+=10
+            f[questionNo] = True
+            connlist[playerNo].send(("Jawaban Benar!").encode())
+            time.sleep(0.1)
+        else :
+            connlist[playerNo].send(("Kurang beruntung :(").encode())
+            time.sleep(0.1)
+    else :
+        connlist[playerNo].send(("Jawaban Salah!").encode())
+        time.sleep(0.1)
+
+def sendallScore(connlist):
+    global score
+    for i, conn in enumerate(connlist):
+        conn.send("S")
+        time.sleep(0.1)
+        conn.send("Player "+str(i+1)+", skor kamu adalah: "+str(score[i])+"\n")
+        time.sleep(0.1)
+
+def scoreFinal() :
+    if score[0]>score[1]:
+        print ("Player 1 menang, dengan skor: ", score)
+        conn1.send("MENANG".encode())
+        time.sleep(0.1)
+        conn2.send("KALAH".encode())
+        time.sleep(0.1)
+    elif score[0]<score[1]:
+        print ("Player 2 menang, dengan skor: ", score)
+        conn2.send("MENANG".encode())
+        time.sleep(0.1)
+        conn1.send("KALAH".encode())
+        time.sleep(0.1)
+    else:
+        print ("Skor seimbang, dengan skor: ", score)
+        conn1.send("TIE".encode())
+        time.sleep(0.1)
+        conn2.send("TIE".encode())
+        time.sleep(0.1)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(2)
+print ("Server bound to ", HOST, ":", PORT, "\nConnect both players before continuing...")
+(conn1, addr) = s.accept()
+print ("Connected to Player 1 at ", addr)
+(conn2, addr) = s.accept()
+print ("Connected to Player 2 at ", addr)
+
+connlist = [conn1, conn2]
+
+time.sleep(0.1)
+conn1.send("I".encode())
+conn1.send("Player 1".encode())
+
+time.sleep(0.1)
+conn2.send("I".encode())
+conn2.send("You are Player 2".encode())
+
+for questionNo in range(totalQuestions):
+    conn1.send("Q".encode())
+    time.sleep(0.1)
+    conn2.send("Q".encode())
+    time.sleep(0.1)
+    
+    Soal = "\nPertanyaan Nomor " + str(questionNo+1)
+    conn1.send(Soal.encode())
+    time.sleep(0.1)
+    conn2.send(Soal.encode())
+    time.sleep(0.1)
+
+    ques = f.readline()+f.readline()+f.readline()+f.readline()+f.readline()
+    print (ques)
+    ans = f.readline()
+    
+    playerThread1 = threading.Thread(target = askQuestion, name = "Thread1", args = (connlist, 0, ques, ans, questionNo))
+    playerThread2 = threading.Thread(target = askQuestion, name = "Thread2", args = (connlist, 1, ques, ans, questionNo))
+    playerThread1.start()
+    playerThread2.start()
+    playerThread1.join()
+    playerThread2.join()
+
+    if questionNo<totalQuestions:
+        sendallScore(connlist)
+
+time.sleep(0.1)
+conn1.send("F".encode())
+time.sleep(0.1)
+conn2.send("F".encode())
+scoreFinal()
+s.close()
